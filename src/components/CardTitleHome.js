b80c586cf9eb059eaf4ac67383b3e94f899fa6c8
Richona <105828822+Richona@users.noreply.github.com>
2022-12-06 18:31:21 -0300

    microdesafio 2 completado

---
+++ src/components/CardTitleHome.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function CardTitleHome() {
+    return (
+        <div class="col-lg-3 col-md-6 mb-4">
+            <div class="card h-100">
+                <img class="card-img-top" src="" alt="" />
+                <div class="card-body">
+                    <h4 class="card-title">Titulo</h4>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import CardTitleHome from './CardTitleHome'
+
+export default function Home() {
+    return (
+        <div class="container">
+
+            <div class="row text-center">
+
+                <CardTitleHome />
+                <CardTitleHome />
+                <CardTitleHome />
+                <CardTitleHome />
+                
+
+            </div>
+
+        </div>
+    )
+}
